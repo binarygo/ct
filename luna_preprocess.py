b9c736742a4c42ab0315f8f5fb6c7a1c8ce93da3
binarygo <binarygo>
2017-02-02 20:52:36 -0500

    Dump lungmask.

--- luna_preprocess.py
+++ luna_preprocess.py
@@ -18,35 +18,59 @@ _OUTPUT_DIR = '../LUNA16/output'
 def get_file_id(f):
     return os.path.basename(f)[:-4]
 
-def process_file(f, annt_df):
-    itk = sitk.ReadImage(f)
-    image = sitk.GetArrayFromImage(itk)
-    spacing = 1
-    image_resampled = util.resample(image, itk.GetSpacing()[::-1], spacing)
 
-    lung_mask = util.segment_lung_mask_v2(image_resampled, spacing)
-    lung_image = util.apply_mask(image_resampled, lung_mask)
+def get_file_list():
+    return glob(os.path.join(_DATA_DIR, '*.mhd'))
 
-    df = annt_df[annt_df.file==f]
-    nodule_masks = luna_util.extract_nodule_masks(
-        lung_image, spacing, np.array(itk.GetOrigin()), df)
 
-    file_id = get_file_id(f)
-    for i, nodule_mask in enumerate(nodule_masks):
-        tmp_image, tmp_nodule_mask = nodule_mask
-        np.save(os.path.join(_OUTPUT_DIR, "%s_image_%s.npy"%(file_id, i)), tmp_image)
-        np.save(os.path.join(_OUTPUT_DIR, "%s_nodule_mask_%s.npy"%(file_id, i)), tmp_nodule_mask)
-        
+def get_file_dict(file_list):
+    return  {get_file_id(f) : f for f in file_list}
 
-if __name__ == '__main__':
-    file_list = glob(os.path.join(_DATA_DIR, '*.mhd'))
-    file_dict = {get_file_id(f) : f for f in file_list}
 
+def get_annt_df(file_dict):
     annt_df = pd.read_csv(_ANNOTATION_CSV)
     annt_df['file'] = annt_df['seriesuid'].map(lambda suid: file_dict.get(suid))
     annt_df = annt_df.dropna()
+    return annt_df
+
+
+def process_file(f, annt_df, spacing, dump=False):
+    itk = sitk.ReadImage(f)
+    origin = itk.GetOrigin()
+    old_spacing = itk.GetSpacing()
+
+    image = sitk.GetArrayFromImage(itk)
+    image_resampled = util.resample(image, old_spacing[::-1], spacing)
+    num_z, height, width = image_resampled.shape
+
+    lung_mask = util.segment_lung_mask_v2(image_resampled, spacing)
+
+    df = annt_df[annt_df.file==f]
+    d_iz = int(round(old_spacing[2] / spacing))
+    nodule_masks = luna_util.extract_nodule_masks(
+        num_z, height, width, spacing, [-d_iz, 0, d_iz], np.array(origin), df)
+
+    file_id = get_file_id(f)
+    for i, nodule_mask_info in enumerate(nodule_masks):
+        nodule_mask, izs = nodule_mask_info
+        tmp_image = image_resampled[izs]
+        tmp_lung_mask = lung_mask[izs]
+        if dump:
+            np.save(os.path.join(_OUTPUT_DIR, "%s_image_%s.npy"%(file_id, i)),
+                    tmp_image)
+            np.save(os.path.join(_OUTPUT_DIR, "%s_lung_mask_%s.npy"%(file_id, i)),
+                    tmp_lung_mask)
+            np.save(os.path.join(_OUTPUT_DIR, "%s_nodule_mask_%s.npy"%(file_id, i)),
+                    nodule_mask)
+    return tmp_image, tmp_lung_mask, nodule_mask
+
+
+if __name__ == '__main__':
+    file_list = get_file_list()
+    file_dict = get_file_dict(file_list)
+    annt_df = get_annt_df(file_dict)
 
     for i, f in enumerate(file_list):
         print '========== process %s'%f
         sys.stdout.flush()
-        process_file(f, annt_df)
+        process_file(f, annt_df, 1, True)
--- luna_util.py
+++ luna_util.py
@@ -14,19 +14,21 @@ def make_nodule_mask(width, height, spacing, z, origin, center, diam):
       center: nodule center x, y, z (mm).
       diam: nodule diameter (mm).
     """
-    mask = np.zeros([height,width], dtype=np.bool) # 0's everywhere except nodule swapping x,y to match img
-    #convert to nodule space from world coordinates
+    mask = np.zeros([height,width], dtype=np.bool)
+    # 0's everywhere except nodule swapping x,y to match img
+    # convert to nodule space from world coordinates
 
     origin = np.asarray(origin)
     center = np.asarray(center)
 
     # Defining the voxel range in which the nodule falls
     v_center = (center - origin) / spacing
-    v_diam = int(diam / spacing + 5)
-    v_xmin = np.max([0,int(v_center[0]-v_diam)-5])
-    v_xmax = np.min([width-1,int(v_center[0]+v_diam)+5])
-    v_ymin = np.max([0,int(v_center[1]-v_diam)-5]) 
-    v_ymax = np.min([height-1,int(v_center[1]+v_diam)+5])
+    pad = 5
+    v_diam = int(diam / spacing + pad)
+    v_xmin = np.max([0,int(v_center[0]-v_diam)-pad])
+    v_xmax = np.min([width-1,int(v_center[0]+v_diam)+pad])
+    v_ymin = np.max([0,int(v_center[1]-v_diam)-pad]) 
+    v_ymax = np.min([height-1,int(v_center[1]+v_diam)+pad])
 
     v_xrange = range(v_xmin,v_xmax+1)
     v_yrange = range(v_ymin,v_ymax+1)
@@ -45,8 +47,7 @@ def make_nodule_mask(width, height, spacing, z, origin, center, diam):
     return mask
 
 
-def extract_nodule_masks(image, spacing, origin, nodule_df):
-    num_z, height, width = image.shape
+def extract_nodule_masks(num_z, height, width, spacing, d_izs, origin, nodule_df):
     origin = np.asarray(origin)
     ans = []
     for node_idx, cur_row in nodule_df.iterrows():
@@ -57,12 +58,10 @@ def extract_nodule_masks(image, spacing, origin, nodule_df):
         center = np.array([node_x, node_y, node_z])
         v_center = np.rint((center-origin)/spacing)
 
-        imgs = []
         masks = []
-        i_zs = np.arange(int(v_center[2])-1, int(v_center[2])+2).clip(0, num_z-1)
-        for i, i_z in enumerate(i_zs):
-            imgs.append(image[i_z])
+        izs = (np.asarray(d_izs) + v_center[2]).astype(np.int).clip(0, num_z-1)
+        for i, iz in enumerate(izs):
             masks.append(make_nodule_mask(
-                width, height, spacing, i_z*spacing+origin[2], origin, center, diam))
-        ans.append((np.stack(imgs), np.stack(masks)))
+                width, height, spacing, iz*spacing+origin[2], origin, center, diam))
+        ans.append((np.stack(masks), izs))
     return ans
